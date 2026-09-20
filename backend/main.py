2d8f5d980cb81410ee05c60f827286fddb511f72
mohankalimuthu <mohankalimuthu2004@gmail.com>
2026-09-20 17:43:14 +0530

    add some restriction

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,7 @@
 import shutil
 import uuid
-
+import os
+import uvicorn
 from pathlib import Path
 
 from fastapi import (
@@ -419,14 +420,8 @@ if __name__ == "__main__":
 
     import uvicorn
 
-
     uvicorn.run(
-
         "main:app",
-
-        host="127.0.0.1",
-
-        port=8000,
-
-        reload=True
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 8000))
     )
